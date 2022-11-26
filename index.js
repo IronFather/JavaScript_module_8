54e6936b631b01f53acc6d744c8dcf319004e6e2
Yurii Yashchuk <106056914+IronFather@users.noreply.github.com>
2022-11-26 16:26:49 +0200

    4

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 console.log(`привіт node.js`);
 
+const Joi = require(`joi`);
 
+const password = Joi.string().min(3).max(10).alphanum();
+
+console.log(password.validate(`gerrrvvggbgbgrbg`));
+
+const func = () => {
+    console.log(`It's great day`);
+}
