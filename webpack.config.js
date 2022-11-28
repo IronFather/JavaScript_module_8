13cce1d8e7c15d10cffb690fcef002dfb35b9984
Yurii Yashchuk <106056914+IronFather@users.noreply.github.com>
2022-11-28 17:30:20 +0200

    2wp

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,29 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: 'src/index.js',
     mode: `development`,
     output: {
     filename: 'my-bundle.js',
     path: path.resolve(__dirname, 'build'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/i,
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                    presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ],
     },
 };
