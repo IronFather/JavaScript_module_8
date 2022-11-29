5e8e79ac7bde213cda18cb36c0b8aa66c8580899
Yurii Yashchuk <106056914+IronFather@users.noreply.github.com>
2022-11-29 13:51:16 +0200

    restored repository

---
+++ build/my-bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError [ERR_MODULE_NOT_FOUND]: Cannot find package '@babel/preset-env' imported from C:\\\\Users\\\\38095\\\\Desktop\\\\iamstudent\\\\JavaScript_module_8\\\\babel-virtual-resolve-base.js\\n    at new NodeError (C:\\\\Users\\\\38095\\\\Desktop\\\\iamstudent\\\\JavaScript_module_8\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\vendor\\\\import-meta-resolve.js:2327:5)\\n    at packageResolve (C:\\\\Users\\\\38095\\\\Desktop\\\\iamstudent\\\\JavaScript_module_8\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\vendor\\\\import-meta-resolve.js:2906:9)\\n    at moduleResolve (C:\\\\Users\\\\38095\\\\Desktop\\\\iamstudent\\\\JavaScript_module_8\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\vendor\\\\import-meta-resolve.js:2937:18)\\n    at defaultResolve (C:\\\\Users\\\\38095\\\\Desktop\\\\iamstudent\\\\JavaScript_module_8\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\vendor\\\\import-meta-resolve.js:2969:13)\\n    at C:\\\\Users\\\\38095\\\\Desktop\\\\iamstudent\\\\JavaScript_module_8\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\vendor\\\\import-meta-resolve.js:2990:14\\n    at Generator.next (<anonymous>)\\n    at asyncGeneratorStep (C:\\\\Users\\\\38095\\\\Desktop\\\\iamstudent\\\\JavaScript_module_8\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\vendor\\\\import-meta-resolve.js:45:103)\\n    at _next (C:\\\\Users\\\\38095\\\\Desktop\\\\iamstudent\\\\JavaScript_module_8\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\vendor\\\\import-meta-resolve.js:46:194)\\n    at C:\\\\Users\\\\38095\\\\Desktop\\\\iamstudent\\\\JavaScript_module_8\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\vendor\\\\import-meta-resolve.js:46:364\\n    at new Promise (<anonymous>)\");\n\n//# sourceURL=webpack://javascript_module_8/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
